0f5d4242736c54f360ae908288b171f0781a3eaa
felyc <fely.coman@gmail.com>
2025-05-06 10:42:10 +0200

    GenerateVerificationCode completed

--- VerificationProvider/Program.cs
+++ VerificationProvider/Program.cs
@@ -1,13 +1,38 @@
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Diagnostics;
+using VerificationProvider.Data.Contexts;
 
-var builder = FunctionsApplication.CreateBuilder(args);
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services =>
+    {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+        services.AddDbContext<DataContext>(x => x.UseSqlServer(Environment.GetEnvironmentVariable("VerificationRequestDatabase")));
 
-builder.ConfigureFunctionsWebApplication();
 
-// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
-// builder.Services
-//     .AddApplicationInsightsTelemetryWorkerService()
-//     .ConfigureFunctionsApplicationInsights();
+    })
+    .Build();
 
-builder.Build().Run();
+using (var scope = host.Services.CreateScope())
+{
+    try
+    {
+        var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+        var migration = context.Database.GetPendingMigrations();
+        if (migration != null && migration.Any())
+        {
+            context.Database.Migrate();
+        }
+
+    }
+    catch (Exception ex)
+    {
+        Debug.WriteLine($"ERROR : VerificationProvider.Program.cs :: {ex.Message}");
+    }
+}
+host.Run();
